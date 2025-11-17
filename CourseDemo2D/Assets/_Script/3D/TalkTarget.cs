990b489f66f5e1d6ddd7b856687ece48a5d979f1
Temp1113 <ohoo5705@gmail.com>
2025-11-17 09:53:53 +0800

    update dialogue system

---
+++ CourseDemo2D/Assets/_Script/3D/TalkTarget.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkTarget : MonoBehaviour
+{
+    public GameObject dialogueUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //close dialogue panel
+        dialogueUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // if collide with talk target, show dialogue
+        if (other.CompareTag("TalkTarget"))
+        {
+            dialogueUI.SetActive(true);
+        }
+    }
+}
